4d0b291905206751bb15fe511ff63a5d80028093
bramjanssen <bram.janssen@vito.be>
2025-02-13 16:51:53 +0100

    feat: added update to parent catalogue

--- lib/earthcode/publish.ts
+++ lib/earthcode/publish.ts
@@ -3,36 +3,35 @@ import {JobSchemaInfo, SchemaType} from "./schema.model";
 import {getOpenEOJobResults} from "../openeo/jobs";
 import {OpenEOBackend} from "../openeo/jobs.models";
 import {createProductCollection} from "./schema";
-import {createFile} from "../github/files";
+import {createFile, getFile, updateFile} from "../github/files";
 import {createPR} from "../github/pr";
-
 
 
 export const publishSchemas = async function*(token: string, branch: string, backend: OpenEOBackend, schemas: JobSchemaInfo[]) {
 
-    const steps = schemas.length + 2;
+    const steps = (schemas.length * 2) + 3;
     let stepCount = 1;
-    let progress = 1;
+    const products: JobSchemaInfo[] = [];
 
-    const updateProgress = () => progress = Math.round(((stepCount++) / steps) * 100);
+    const getProgress = () => Math.round(((stepCount++) / steps) * 100);
 
     if (!token || !branch || !backend || !schemas) {
         yield { status: "error", message: "Missing or invalid parameters", progress: 0 };
         return;
     }
 
-    yield { status: 'processing', message: "Creating branch", progress};
+    yield { status: 'processing', message: "Creating branch", progress: getProgress()};
     await createBranch(token, branch);
 
     try {
         let jobIdx = 1;
         for (const schema of schemas) {
-            yield { status: "processing", message: `Fetching job info for ${schema.job.title} (${jobIdx}/${schemas.length})`, progress };
+            yield { status: "processing", message: `Fetching job info for ${schema.job.title} (${jobIdx}/${schemas.length})`, progress: getProgress() };
 
             const details = await getOpenEOJobResults(backend, schema.job.id);
 
             if (schema.type === SchemaType.PRODUCT) {
-                yield { status: "progress", message: `Processing product ${schema.id}`, progress };
+                yield { status: "progress", message: `Processing product ${schema.id}`, progress: getProgress() };
                 const product = createProductCollection(schema.id, schema.project, details);
                 await createFile(
                     token,
@@ -41,12 +40,19 @@ export const publishSchemas = async function*(token: string, branch: string, bac
                     `Added product from openEO job ${schema.job.title} (${schema.job.id})`,
                     product
                 );
+                products.push(schema);
             }
-            updateProgress();
             jobIdx++;
         }
 
-        yield { status: "progress", message: "Creating PR...", progress };
+        if (products.length > 0) {
+            yield { status: "progress", message: `Registering ${products.length} products in parent catalog`, progress: getProgress() };
+            await registerParentCatalogue(token, 'products/catalog.json', branch, schemas.filter(s => s.type === SchemaType.PRODUCT));
+        } else {
+            yield { status: "progress", message: `No products found to register in parent catalog`, progress: getProgress() };
+        }
+
+        yield { status: "progress", message: "Creating PR...", progress: getProgress() };
         await createPR(token, branch, backend, schemas.map(s => s.job));
 
         yield { status: "complete", message: "Publishing complete!", progress: 100};
@@ -55,5 +61,20 @@ export const publishSchemas = async function*(token: string, branch: string, bac
         yield { status: "error", message: `Failed to publish to EarthCODE Open Science Catalog: ${error}`, progress: 0 };
         await deleteBranch(token, branch);
     }
+}
 
+const registerParentCatalogue = async (token: string, path: string, branch: string, schemas: JobSchemaInfo[]) => {
+
+    const {sha, content} = await getFile(token, path, branch);
+
+    for (const schema of schemas) {
+        content.links.push({
+            rel: "child",
+            href: `./${schema.id}/collection.json`,
+            type: "application/json",
+            title: schema.job.title
+        })
+    }
+
+    await updateFile(token, path, branch, sha, 'Updated parent collection', content);
 }
--- lib/github/files.ts
+++ lib/github/files.ts
@@ -1,6 +1,5 @@
 'use client';
 
-import {OpenEOJobDetails} from "../openeo/jobs.models";
 import {getOctokit, GITHUB_OWNER, GITHUB_REPO} from "./index";
 import {EarthCODEProduct} from "../earthcode/product.models";
 
@@ -16,6 +15,44 @@ export const createFile = async (token: string, branch: string, path: string, me
             message,
             content,
             branch
+        });
+    } else {
+        throw Error('GitHub repository not configured!');
+    }
+}
+
+export const getFile = async (token: string, path: string, branch: string) => {
+
+    if (GITHUB_OWNER && GITHUB_REPO) {
+        const {data: fileData} = await getOctokit(token).repos.getContent({
+            owner: GITHUB_OWNER,
+            repo: GITHUB_REPO,
+            path,
+            ref: branch,
+        });
+
+        return {
+            sha: fileData.sha,
+            content: JSON.parse(Buffer.from(fileData.content, "base64").toString('utf-8')),
+        };
+
+    } else {
+        throw Error('GitHub repository not configured!');
+    }
+}
+
+
+export const updateFile = async (token: string, path: string, branch: string, sha: string, message: string, content: any) => {
+    if (GITHUB_OWNER && GITHUB_REPO) {
+        const contentBase64 = Buffer.from(JSON.stringify(content, null, 2)).toString('base64');
+        await getOctokit(token).repos.createOrUpdateFileContents({
+            owner: GITHUB_OWNER,
+            repo: GITHUB_REPO,
+            path,
+            message,
+            content: contentBase64,
+            sha,
+            branch,
         });
     } else {
         throw Error('GitHub repository not configured!');
