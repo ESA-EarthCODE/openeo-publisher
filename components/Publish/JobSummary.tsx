62337909c96bf5b3a5d9d5cfc16523dcd44e9e35
bramjanssen <bram.janssen@vito.be>
2025-02-13 11:22:57 +0100

    style: fixed styling of job summary

--- components/Publish/JobSummary.tsx
+++ components/Publish/JobSummary.tsx
@@ -10,10 +10,10 @@ interface JobSummaryProps {
 
 export const JobSummary = ({ job, selectedSchemas, onSchemaChange }: JobSummaryProps) => {
     return (
-        <div className="flex items-center justify-between w-full bg-neutral-100 rounded-t-lg px-4 py-4">
-            <span>{job.title}</span>
+        <div className="flex items-center justify-between w-full bg-primary text-white rounded-lg shadow-lg px-4 py-4">
+            <span className='font-bold text-sm'>{job.title}</span>
             <FormControl>
-                <InputLabel id={`select-as-${job.id}-label`}>Publish as</InputLabel>
+                <InputLabel id={`select-as-${job.id}-label`} sx={{ color: 'white', '&.Mui-focused': { color: 'white'}}}>Publish as</InputLabel>
                 <Select
                     labelId={`select-as-${job.id}-label`}
                     label="Index as"
@@ -21,6 +21,16 @@ export const JobSummary = ({ job, selectedSchemas, onSchemaChange }: JobSummaryP
                     value={selectedSchemas}
                     onChange={(e) => onSchemaChange(job, e.target.value as SchemaType[])}
                     className="min-w-64"
+                    sx={{
+                        color: 'white',
+                        backgroundColor: 'transparent', // Ensure background remains clear
+                        borderColor: 'white',
+                        '.MuiOutlinedInput-notchedOutline': { borderColor: 'white' }, // Ensures white border
+                        '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+                        '&.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+                        '.MuiSvgIcon-root': { color: 'white' }, // Ensures dropdown arrow is white
+                    }}
+                    size='small'
                 >
                     {Object.values(SchemaType).map((type) => (
                         <MenuItem key={type} value={type}>
