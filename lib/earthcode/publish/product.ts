b0e42468fc89cc69e26bdb7e96d43c5a16cfb2e3
bramjanssen <brm.janssen@gmail.com>
2026-03-18 15:55:29 +0100

    feat: upload products to an S3 and use those links

--- lib/earthcode/publish/product.ts
+++ lib/earthcode/publish/product.ts
@@ -4,6 +4,9 @@ import {OpenEOBackend} from "lib/openeo/jobs.models";
 import {createProductCollection} from "../schema";
 import {createJSONFile} from "lib/github/files";
 import {EarthCODEProduct} from "../concepts.models";
+import {uploadPublicFileToS3} from "./storage";
+
+const PRODUCT_ASSETS_BUCKET = "products";
 
 export const publishProduct = async (
     schema: ProductInfo,
@@ -12,10 +15,26 @@ export const publishProduct = async (
     branch: string
 ): Promise<EarthCODEProduct> => {
     const results = await getOpenEOJobResults(backend, schema.job.id);
-    const product = createProductCollection(
-        schema.id, schema.title, schema.description, schema.project || {id: '', title: ''}, schema.themes, schema.assets, results
+
+    const copiedAssets = await Promise.all(
+        schema.assets.map(async (asset) => {
+            const uploadedUrl = await uploadPublicFileToS3(
+                asset.url,
+                `${schema.id}`,
+                PRODUCT_ASSETS_BUCKET,
+                true
+            );
+
+            return {
+                ...asset,
+                url: uploadedUrl,
+            };
+        })
     );
 
+    const product = createProductCollection(
+        schema.id, schema.title, schema.description, schema.project || {id: '', title: ''}, schema.themes, copiedAssets, results
+    );
     await createJSONFile(
         token,
         branch,
